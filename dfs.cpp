1f8f9bcb69bc5ec1881bd821ac44f33cebadf135
Tim Hutton <tihutt@microsoft.com>
2022-02-17 22:08:48 +0100

    Using emscripten::val to return a Float32Array with zero copy.

--- dfs.cpp
+++ dfs.cpp
@@ -1,43 +1,14 @@
 // Emscripten
 #include <emscripten/bind.h>
-using namespace emscripten;
+#include <emscripten/val.h>
 
-// stdlib
-#include <cstdint>
-
-class Buffer
-{
-    public:
-        Buffer(int n)
-            : m_n(n)
-        {
-            m_arr = new uint8_t[ n ];
-            m_arr[0] = static_cast<uint8_t>(111);
-            m_arr[1] = static_cast<uint8_t>(75);
-            m_arr[2] = static_cast<uint8_t>(33);
-        }
-        int size() { return m_n; }
-        int pointer()
-        {
-            return reinterpret_cast<int>(m_arr);
-        }
-
-    private:
-        int m_n;
-        uint8_t* m_arr;
-};
-
-Buffer compute()
-{
-    Buffer b(7);
-    return b;
+emscripten::val compute() {
+    std::vector<float> b;
+    b.push_back( 43.5f );
+    b.push_back( 113.01f );
+    return emscripten::val( emscripten::typed_memory_view( b.size(), &b.front() ) );
 }
 
-EMSCRIPTEN_BINDINGS(my_module) {
-    class_<Buffer>("Buffer")
-    .constructor<int>()
-    .function("size", &Buffer::size)
-    .function("pointer", &Buffer::pointer);
-
-    function("compute", &compute);
+EMSCRIPTEN_BINDINGS(dfs) {
+    emscripten::function("compute", &compute);
 }
