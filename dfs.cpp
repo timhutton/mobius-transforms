37377aec2bc7662e36874cc05cdb8f96d7f2a92a
Tim Hutton <tihutt@microsoft.com>
2022-02-17 22:47:55 +0100

    Pass LineSegments buffer directly from C++ to THREE.js

--- dfs.cpp
+++ dfs.cpp
@@ -3,13 +3,19 @@
 #include <emscripten/val.h>
 
 // stdlib
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <vector>
+
 
 emscripten::val compute() {
     std::vector<float> b;
-    b.push_back( 43.5f );
-    b.push_back( 113.01f );
-    return emscripten::val( emscripten::typed_memory_view( b.size(), &b.front() ) );
+    for(float theta = 0.0f; theta < M_PI; theta += 0.1f) {
+        b.push_back( std::cos( theta ) );
+        b.push_back( std::sin( theta ) );
+        b.push_back( 0.0f );
+    }
+    return emscripten::val( emscripten::typed_memory_view( b.size(), b.data() ) );
 }
 
 EMSCRIPTEN_BINDINGS( dfs )
