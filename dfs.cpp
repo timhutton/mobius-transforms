001f453bdb01e26b5a7b84efb6a7ff3dc82c2e3c
Tim Hutton <tihutt@microsoft.com>
2022-02-17 22:16:45 +0100

    Updated build instructions

--- dfs.cpp
+++ dfs.cpp
@@ -2,6 +2,9 @@
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 
+// stdlib
+#include <vector>
+
 emscripten::val compute() {
     std::vector<float> b;
     b.push_back( 43.5f );
@@ -9,6 +12,7 @@ emscripten::val compute() {
     return emscripten::val( emscripten::typed_memory_view( b.size(), &b.front() ) );
 }
 
-EMSCRIPTEN_BINDINGS(dfs) {
+EMSCRIPTEN_BINDINGS( dfs )
+{
     emscripten::function("compute", &compute);
 }
