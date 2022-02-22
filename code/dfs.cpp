99c71b1d1ef61a9d10134e42b0fc744ed7aee4be
Tim Hutton <tihutt@microsoft.com>
2022-02-22 17:44:19 +0000

    Pass all control points to C++

--- code/dfs.cpp
+++ code/dfs.cpp
@@ -8,9 +8,22 @@
 #include <vector>
 
 std::vector<float> line_segments;
+std::vector<Complex> control_points;
 
-emscripten::val compute(float x, float y) {
+void set_number_of_control_points(int n)
+{
+    control_points.resize(n);
+}
 
+void set_control_point(int i, float re, float im)
+{
+    control_points[i] = { re, im };
+}
+
+emscripten::val compute()
+{
+    const float x = control_points[0].real();
+    const float y = control_points[0].imag();
     // placeholder: output line segments for a circle
     line_segments.clear();
     for(float theta = 0.0f; theta < 2.0 * M_PI; theta += 0.01f) {
@@ -25,5 +38,7 @@ emscripten::val compute(float x, float y) {
 
 EMSCRIPTEN_BINDINGS( dfs )
 {
+    emscripten::function("set_number_of_control_points", &set_number_of_control_points);
+    emscripten::function("set_control_point", &set_control_point);
     emscripten::function("compute", &compute);
 }
