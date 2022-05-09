683042a8b5ccd7bea2b7d5816ddf3913dab9a0d6
Tim Hutton <tihutt@microsoft.com>
2022-05-09 23:45:02 +0100

    Added missing license comments.

--- code/mobius.h
+++ code/mobius.h
@@ -1,3 +1,21 @@
+/*
+    mobius-transforms - Exploring Möbius transformations and implementing the book Indra's Pearls
+    Copyright (C) 2022 Tim J. Hutton
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Eigen
 #include <Eigen/Dense>
 
@@ -39,7 +57,7 @@ std::array<Complex, 2> get_mobius_fixed_points(const Mobius& m)
     else { return { z_minus, z_plus }; }
 }
 
-std::array<Complex, 2> complex_solve_quadratic(const Complex& a, const Complex& b, const Complex& c )
+std::array<Complex, 2> complex_solve_quadratic(const Complex& a, const Complex& b, const Complex& c)
 {
     // return both solutions of ax^2 + bx + c = 0
     const Complex sqrt_term = sqrt( b * b - 4.0f * a * c );
